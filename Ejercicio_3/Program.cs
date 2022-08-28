7dca7696d1fe36093dd69072f23c95068427e963
Usuario <Usuario@DESKTOP-BIRK270>
2022-08-28 11:04:26 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ejercicio_3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(factorial(3));
+            
+        }
+        public static int factorial(int num)
+        {
+            int resultado = num;
+            for (int x = num - 1; x > 1; x--)
+            {
+                resultado *= x;
+            }
+            return resultado;
+        }
+
+     
+    }
+
+
+}
